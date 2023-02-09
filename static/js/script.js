db282c480c712d1be695195116a1792853905df0
Fatima Masood <63922721+fatima22masood@users.noreply.github.com>
2023-02-09 08:53:40 +0500

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+
+let accordion = document.querySelectorAll(
+  ".faq .accordion-container .accordion"
+);
+
+accordion.forEach((acco) => {
+  acco.onclick = () => {
+    accordion.forEach((dion) => dion.classList.remove("active"));
+    acco.classList.toggle("active");
+  };
+});
+
+document.querySelector(".load-more .btn").onclick = () => {
+  document.querySelectorAll(".contact .box-container .hide").forEach((show) => {
+    show.style.display = "block";
+  });
+  document.querySelector(".load-more .btn").style.display = "none";
+};
